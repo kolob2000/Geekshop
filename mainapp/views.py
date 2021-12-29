b3835a1bdeef9c0cb036bc249eba881c00451336
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-12-29 17:07:28 +0300

    add second home work (#2)
    * add second home work

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,109 @@
+import json
+
 from django.shortcuts import render
+
 
 # Create your views here.
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    # title = 'главная'
+    # products = [
+    #     {
+    #         'img': 'img/product-1.jpg',
+    #         'icon': 'img/icon-hover.png',
+    #         'title': 'Отличный стул',
+    #         'text': 'Расположитесь комфортно',
+    #     },
+    #     {
+    #         'img': 'img/product-2.jpg',
+    #         'icon': 'img/icon-hover.png',
+    #         'title': 'Стул повышенного качества',
+    #         'text': 'Не оторваться',
+    #     },
+    # ]
+    # context = {
+    #     'title': title,
+    #     'products': products,
+    # }
+    with open('mainapp/main.json', 'r') as f:
+        context = json.loads(f.read())
+    return render(request, 'mainapp/index.html', context=context)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    # title = 'продукты'
+    #
+    # menu_links = [
+    #     {'href': 'products', 'name': 'Все'},
+    #     {'href': 'house', 'name': 'Дом'},
+    #     {'href': 'office', 'name': 'Офис'},
+    #     {'href': 'modern', 'name': 'Модерн'},
+    #     {'href': 'classic', 'name': 'Классика'},
+    # ]
+    # slider_product = [
+    #     {'img': 'img/controll.jpg', },
+    #     {'img': 'img/controll1.jpg', },
+    #     {'img': 'img/controll2.jpg', },
+    # ]
+    # rel_products = [
+    #     {
+    #         'img': 'img/product-11.jpg',
+    #         'icon': 'img/icon-hover.png',
+    #         'title': 'Люстра повышенного качества',
+    #         'text': 'Не оторваться',
+    #     },
+    #     {
+    #         'img': 'img/product-21.jpg',
+    #         'icon': 'img/icon-hover.png',
+    #         'title': 'Стул повышенного качества',
+    #         'text': 'Не оторваться',
+    #     },
+    #     {
+    #         'img': 'img/product-31.jpg',
+    #         'icon': 'img/icon-hover.png',
+    #         'title': 'Торшер повышенного качества',
+    #         'text': 'Не оторваться',
+    #     },
+    # ]
+    # context = {
+    #     'menu_links': menu_links,
+    #     'title': title,
+    #     'slider_product': slider_product,
+    #     'rel_products': rel_products,
+    # }
+    with open('mainapp/products.json', 'r') as f:
+        context = json.loads(f.read())
+    return render(request, 'mainapp/products.html', context=context)
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    # title = 'контакты и т.д.'
+    # contacts = [
+    #     {
+    #         'city': 'Петропавловск-Камчатский',
+    #         'phone': '+7-888-888-8888',
+    #         'email': 'info@geekshop.ru',
+    #         'address': 'ул. Труда 33',
+    #     },
+    #     {
+    #         'city': 'Санкт-Петербург',
+    #         'phone': '+7-888-888-8888',
+    #         'email': 'info@geekshop.ru',
+    #         'address': 'ул. Русановская',
+    #     },
+    #     {
+    #         'city': 'Барнаул',
+    #         'phone': '+7-888-888-8888',
+    #         'email': 'info@geekshop.ru',
+    #         'address': 'ул. Ленина',
+    #     },
+    #
+    # ]
+    # context = {
+    #     'title': title,
+    #     'contacts': contacts
+    # }
+    with open('mainapp/contact.json', 'r') as f:
+        context = json.loads(f.read())
+    return render(request, 'mainapp/contact.html', context=context)
