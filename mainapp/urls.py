17b2c065a179f310019d4a064f3ec0169585b8e1
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2022-01-02 22:52:55 +0300

    add third lesson

---
+++ mainapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# import views as mainapp
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
+urlpatterns = [
+    path('', mainapp.products, name='products'),
+    path('<slug:slug>/', mainapp.products, name='category'),
+]
