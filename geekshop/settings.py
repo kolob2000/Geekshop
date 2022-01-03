9a94cc349cbd0c38512cc23404ea7f591a2a5c31
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2022-01-03 17:44:15 +0300

    Lesson 3 (#3)
    * edits
    
    * add third lesson
    
    * edits

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp'
+    'mainapp.apps.MainappConfig',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'geekshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -100,11 +97,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -114,12 +110,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,109 +1,45 @@
-import json
-
 from django.shortcuts import render
 
-
-# Create your views here.
+from .models import Product, Category, Contact
 
 
 def main(request):
-    # title = 'главная'
-    # products = [
-    #     {
-    #         'img': 'img/product-1.jpg',
-    #         'icon': 'img/icon-hover.png',
-    #         'title': 'Отличный стул',
-    #         'text': 'Расположитесь комфортно',
-    #     },
-    #     {
-    #         'img': 'img/product-2.jpg',
-    #         'icon': 'img/icon-hover.png',
-    #         'title': 'Стул повышенного качества',
-    #         'text': 'Не оторваться',
-    #     },
-    # ]
-    # context = {
-    #     'title': title,
-    #     'products': products,
-    # }
-    with open('mainapp/main.json', 'r') as f:
-        context = json.loads(f.read())
+    title = 'главная'
+    context = {
+        'title': title,
+        'products': Product.objects.all().order_by('?')[:4],
+    }
     return render(request, 'mainapp/index.html', context=context)
 
 
-def products(request):
-    # title = 'продукты'
-    #
-    # menu_links = [
-    #     {'href': 'products', 'name': 'Все'},
-    #     {'href': 'house', 'name': 'Дом'},
-    #     {'href': 'office', 'name': 'Офис'},
-    #     {'href': 'modern', 'name': 'Модерн'},
-    #     {'href': 'classic', 'name': 'Классика'},
-    # ]
-    # slider_product = [
-    #     {'img': 'img/controll.jpg', },
-    #     {'img': 'img/controll1.jpg', },
-    #     {'img': 'img/controll2.jpg', },
-    # ]
-    # rel_products = [
-    #     {
-    #         'img': 'img/product-11.jpg',
-    #         'icon': 'img/icon-hover.png',
-    #         'title': 'Люстра повышенного качества',
-    #         'text': 'Не оторваться',
-    #     },
-    #     {
-    #         'img': 'img/product-21.jpg',
-    #         'icon': 'img/icon-hover.png',
-    #         'title': 'Стул повышенного качества',
-    #         'text': 'Не оторваться',
-    #     },
-    #     {
-    #         'img': 'img/product-31.jpg',
-    #         'icon': 'img/icon-hover.png',
-    #         'title': 'Торшер повышенного качества',
-    #         'text': 'Не оторваться',
-    #     },
-    # ]
-    # context = {
-    #     'menu_links': menu_links,
-    #     'title': title,
-    #     'slider_product': slider_product,
-    #     'rel_products': rel_products,
-    # }
-    with open('mainapp/products.json', 'r') as f:
-        context = json.loads(f.read())
+def products(request, slug=None):
+    title = 'продукты'
+    context = {
+        'menu_links': Category.objects.all(),
+        'products': Product.objects.all(),
+        'title': title,
+        'rel_products': Product.objects.all().order_by('?')[:3],
+    }
+
     return render(request, 'mainapp/products.html', context=context)
 
 
 def contact(request):
-    # title = 'контакты и т.д.'
-    # contacts = [
-    #     {
-    #         'city': 'Петропавловск-Камчатский',
-    #         'phone': '+7-888-888-8888',
-    #         'email': 'info@geekshop.ru',
-    #         'address': 'ул. Труда 33',
-    #     },
-    #     {
-    #         'city': 'Санкт-Петербург',
-    #         'phone': '+7-888-888-8888',
-    #         'email': 'info@geekshop.ru',
-    #         'address': 'ул. Русановская',
-    #     },
-    #     {
-    #         'city': 'Барнаул',
-    #         'phone': '+7-888-888-8888',
-    #         'email': 'info@geekshop.ru',
-    #         'address': 'ул. Ленина',
-    #     },
-    #
-    # ]
-    # context = {
-    #     'title': title,
-    #     'contacts': contacts
-    # }
-    with open('mainapp/contact.json', 'r') as f:
-        context = json.loads(f.read())
+    title = 'контакты и т.д.'
+    context = {
+        'title': title,
+        'contacts': Contact.objects.all()
+    }
+
     return render(request, 'mainapp/contact.html', context=context)
+
+
+def product_detail(request, category, product):
+    product = Product.objects.get(slug=product)
+    context = {
+        'title': product.title,
+        'product': product,
+        'menu_links': Category.objects.all(),
+        'rel_products': Product.objects.all().order_by('?')[:3],
+    }
+    return render(request, 'mainapp/product_detail.html', context=context)
