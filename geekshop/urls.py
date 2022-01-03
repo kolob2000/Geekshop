c9ae8c1e1c0559d2cfba2c5e571cdfa9218ff271
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2022-01-03 17:44:15 +0300

    Lesson 3 (#3)
    * edits
    
    * add third lesson
    
    * edits

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,17 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
 import mainapp.views as mainapp
+from django.conf import settings
+
+# from mainapp.models import Category
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.main, name='main'),
-    path('products/', mainapp.products, name='products'),
-    path('house/', mainapp.products, name='house'),
-    path('office/', mainapp.products, name='office'),
-    path('modern/', mainapp.products, name='modern'),
-    path('classic/', mainapp.products, name='classic'),
+    path("products/", include("mainapp.urls", namespace="products")),
     path('contact/', mainapp.contact, name='contact'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
