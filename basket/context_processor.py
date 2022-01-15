1c3b8f7bd1fb937a3e644a43cb4aa0a899d2d8ee
Nick <k.kolabis@mail.ru>
2022-01-15 12:41:34 +0300

    edits

--- basket/context_processor.py
+++ basket/context_processor.py
@@ -4,5 +4,7 @@ from .models import Basket
 
 
 def quantity(request):
-    count = Basket.objects.all().aggregate(Sum('quantity'))
+    if str(request.user) == 'AnonymousUser':
+        return {'basket_quantity': False}
+    count = Basket.objects.all().filter(user=request.user).aggregate(Sum('quantity'))
     return {'basket_quantity': count['quantity__sum']}
