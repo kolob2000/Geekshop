eda5da71889397697b3982020513234efed71039
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2022-02-04 10:04:44 +0300

    Lesson 8 (#8)
    * add eighth lesson

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,8 +4,8 @@ import mainapp.views as mainapp
 
 app_name = 'mainapp'
 urlpatterns = [
-    path('', mainapp.products, name='products'),
-    path('<slug:slug>/', mainapp.products, name='category'),
-    path('<slug:category>/<slug:product>/', mainapp.product_detail, name='product')
+    # path('', mainapp.products, name='products'),
+    path('', mainapp.ProductListView.as_view(), kwargs={'slug': None}, name='products'),
+    path('<slug:slug>/', mainapp.ProductListView.as_view(), name='category'),
+    path('<slug:category_slug>/<slug:slug>/', mainapp.ProductDetailView.as_view(), name='product')
 ]
-
