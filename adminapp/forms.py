c3041c6ed717a3f3694c842de146cedd2492d4cf
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2022-02-04 10:04:44 +0300

    Lesson 8 (#8)
    * add eighth lesson

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -3,7 +3,7 @@ from django.forms.widgets import CheckboxInput
 
 from authapp.forms import ShopUserChangeForm
 from authapp.models import ShopUser
-from mainapp.models import Category
+from mainapp.models import Category, Product
 
 
 class ShopUserAdminChangeForm(ShopUserChangeForm):
@@ -25,6 +25,20 @@ class CategoryEditForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(CategoryEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, CheckboxInput) and field.widget.input_type == 'checkbox':
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductEditForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ProductEditForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             if isinstance(field.widget, CheckboxInput) and field.widget.input_type == 'checkbox':
                 field.widget.attrs['class'] = 'form-check-input'
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,46 +1,98 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Product, Category, Contact
 
 
-def main(request):
-    title = 'главная'
-    context = {
-        'title': title,
-        'products': Product.objects.all().order_by('?')[:4],
-    }
-    return render(request, 'mainapp/index.html', context=context)
+class MainListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'mainapp/index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MainListView, self).get_context_data(**kwargs)
+        context['title'] = 'главная'
+        return context
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('?')[:4]
 
 
-def products(request, slug=None):
-    title = 'продукты'
-    context = {
-        'menu_links': Category.objects.all(),
-        'products': Product.objects.all() if slug is None else Category.objects.get(slug=slug).product_set.all(),
-        'title': title,
-        'rel_products': Product.objects.all().order_by('?')[:3] if slug is None else Category.objects.get(
-            slug=slug).product_set.all().order_by('?')[:3],
-    }
+# def main(request):
+#     title = 'главная'
+#     context = {
+#         'title': title,
+#         'products': Product.objects.all().order_by('?')[:4],
+#     }
+#     return render(request, 'mainapp/index.html', context=context)
 
-    return render(request, 'mainapp/products.html', context=context)
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'mainapp/products.html'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['title'] = 'Продукты'
+        context['menu_links'] = Category.objects.all().filter(is_active=True)
+        context['rel_products'] = Product.objects.all().filter(is_active=True).order_by('?')[:3] if self.kwargs[
+                                                                                                        'slug'] is None else Category.objects.get(
+            slug=self.kwargs['slug']).product_set.all().filter(is_active=True).order_by('?')[:3]
+        return context
+
+    def get_queryset(self):
+        return Product.objects.all().filter(is_active=True) if self.kwargs['slug'] is None else Category.objects.get(
+            slug=self.kwargs['slug']).product_set.all().filter(is_active=True)
 
 
-def contact(request):
-    title = 'контакты и т.д.'
-    context = {
-        'title': title,
-        'contacts': Contact.objects.all()
-    }
+# def products(request, slug=None):
+#     title = 'продукты'
+#     context = {
+#         'menu_links': Category.objects.all(),
+#         'products': Product.objects.all() if slug is None else Category.objects.get(slug=slug).product_set.all(),
+#         'title': title,
+#         'rel_products': Product.objects.all().order_by('?')[:3] if slug is None else Category.objects.get(
+#             slug=slug).product_set.all().order_by('?')[:3],
+#     }
+#
+#     return render(request, 'mainapp/products.html', context=context)
+class ContactListView(ListView):
+    model = Contact
+    template_name = 'mainapp/contact.html'
+    extra_context = {'title': 'Контакты'}
+    context_object_name = 'contacts'
 
-    return render(request, 'mainapp/contact.html', context=context)
+
+# def contact(request):
+#     title = 'контакты и т.д.'
+#     context = {
+#         'title': title,
+#         'contacts': Contact.objects.all()
+#     }
+#
+#     return render(request, 'mainapp/contact.html', context=context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'mainapp/product_detail.html'
+    context_object_name = 'product'
+    print(model)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        context['title'] = context['product'].title
+        context['menu_links'] = Category.objects.all()
+        context['rel_products'] = Product.objects.all().order_by('?')[:3]
+        return context
 
 
-def product_detail(request, category, product):
-    product = Product.objects.get(slug=product)
-    context = {
-        'title': product.title,
-        'product': product,
-        'menu_links': Category.objects.all(),
-        'rel_products': Product.objects.all().order_by('?')[:3],
-    }
-    return render(request, 'mainapp/product_detail.html', context=context)
+# def product_detail(request, category, product):
+#     product = Product.objects.get(slug=product)
+#     context = {
+#         'title': product.title,
+#         'product': product,
+#         'menu_links': Category.objects.all(),
+#         'rel_products': Product.objects.all().order_by('?')[:3],
+#     }
+#     return render(request, 'mainapp/product_detail.html', context=context)
